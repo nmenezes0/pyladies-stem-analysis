5023946bc681db96bf79b9a32618fa37b5ccaba4
Nina Menezes <nmenezes0@example.com>
2025-04-11 19:56:24 +0100

    add data

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import pandas as pd
 import streamlit as st
 
-st.title("Welcome to my PyLadies Lithuania app!")
 
+st.title("Welcome to my Streamlit app!")
 
+df = pd.read_csv("data/kaggle_women_in_stem.csv")
 
-
+st.dataframe(df)
 
 
 
