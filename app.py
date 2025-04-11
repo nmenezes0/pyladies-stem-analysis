21a5ed0a88d66336dfb1d7b6062a76af0b937697
Nina Menezes <nmenezes0@example.com>
2025-04-11 20:19:56 +0100

    Add filters for data

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import pandas as pd
 import streamlit as st
 
 
-st.title("Welcome to my Streamlit app!")
+st.title("Welcome to my PyLadies Lithuania app!")
 
+st.subheader("Women in STEM data")
 df = pd.read_csv("data/kaggle_women_in_stem.csv")
 
+chosen_countries = st.multiselect(label="Select country", options=df["Country"].unique())
+chosen_subjects = st.multiselect(label="Select subject", options=df["STEM Fields"].unique())
+
+df = df[df["Country"].isin(chosen_countries)]
+df = df[df["STEM Fields"].isin(chosen_subjects)]
+
 st.dataframe(df)
-
-
-
-
-
-
