fd5bf875d8e5774175205eaaeab157f9da815db9
Nina Menezes <nmenezes0@example.com>
2025-04-11 22:09:30 +0100

    Add chart

--- app.py
+++ app.py
@@ -2,15 +2,29 @@ import pandas as pd
 import streamlit as st
 
 
+@st.cache_data
+def kaggle_stem_data():
+    df = pd.read_csv("data/kaggle_women_in_stem.csv")
+    return df
+
+
 st.title("Welcome to my PyLadies Lithuania app!")
 
 st.subheader("Women in STEM data")
-df = pd.read_csv("data/kaggle_women_in_stem.csv")
+df = kaggle_stem_data()
+chosen_countries = st.multiselect(
+    label="Select country", options=df["Country"].unique()
+)
+chosen_subjects = st.multiselect(
+    label="Select subject", options=df["STEM Fields"].unique()
+)
+filtered_data = df[
+    (df["Country"].isin(chosen_countries)) & (df["STEM Fields"].isin(chosen_subjects))
+]
+st.dataframe(filtered_data)
 
-chosen_countries = st.multiselect(label="Select country", options=df["Country"].unique())
-chosen_subjects = st.multiselect(label="Select subject", options=df["STEM Fields"].unique())
 
-df = df[df["Country"].isin(chosen_countries)]
-df = df[df["STEM Fields"].isin(chosen_subjects)]
-
-st.dataframe(df)
+st.subheader("Female graduation in maths in Canada")
+df = kaggle_stem_data()
+df = df[(df["Country"] == "Canada") & (df["STEM Fields"] == "Mathematics")]
+st.bar_chart(df, x="Year", y="Female Graduation Rate (%)", stack=False)
