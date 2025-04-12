7a8f4d65235e1736b83f08c283b5c4a46a8e5b5a
Nina Menezes <nmenezes0@example.com>
2025-04-12 14:40:55 +0100

    add some data cleaning

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import plotly.express as px
 import streamlit as st
 
 
 @st.cache_data
 def kaggle_stem_data():
     df = pd.read_csv("data/kaggle_women_in_stem.csv")
-    return df
+    # Take the mean for duplicate rows
+    df_cleaned = df.groupby(["Country", "Year", "STEM Fields"], as_index=False).mean()
+    return df_cleaned
 
 
 st.title("Welcome to my PyLadies Lithuania app!")
@@ -24,7 +27,30 @@ filtered_data = df[
 st.dataframe(filtered_data)
 
 
-st.subheader("Female graduation in maths in Canada")
+st.subheader("Female enrollment & graduation in maths in Canada")
 df = kaggle_stem_data()
-df = df[(df["Country"] == "Canada") & (df["STEM Fields"] == "Mathematics")]
-st.bar_chart(df, x="Year", y="Female Graduation Rate (%)", stack=False)
+
+chosen_countries = st.multiselect(label="Country", options=df["Country"].unique())
+chosen_subject = st.selectbox(label="Subject", options=df["STEM Fields"].unique())
+timeframe = st.slider(
+    label="Time period",
+    min_value=min(df["Year"]),
+    max_value=max(df["Year"]),
+    value=(2010, 2020),
+)
+
+df = df[
+    (df["Country"].isin(chosen_countries))
+    & (df["STEM Fields"] == chosen_subject)
+    & (df["Year"] >= timeframe[0])
+    & (df["Year"] <= timeframe[1])
+]
+
+fig = px.bar(df, x="Year", y="Female Enrollment (%)", color="Country", barmode="group")
+st.plotly_chart(fig)
+fig = px.bar(
+    df, x="Year", y="Female Graduation Rate (%)", color="Country", barmode="group"
+)
+st.plotly_chart(fig)
+
+# TODO - clean data
